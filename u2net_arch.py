abe6bd1ea7b4961de3a8b060de1c38a865292346
Sai Kamal Nannuri <saikamalnannuri@gmail.com>
2025-07-27 21:36:31 +0530

    Update u2net_arch.py

--- u2net_arch.py
+++ u2net_arch.py
@@ -2,123 +2,122 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# --------- Basic Convolution Block ----------
-class REBNCONV(nn.Module):
-    def __init__(self, in_ch=3, out_ch=3, dirate=1):
-        super(REBNCONV, self).__init__()
-        self.conv_s1 = nn.Conv2d(in_ch, out_ch, 3, padding=1*dirate, dilation=1*dirate)
-        self.bn_s1 = nn.BatchNorm2d(out_ch)
-        self.relu_s1 = nn.ReLU(inplace=True)
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
 
     def forward(self, x):
-        hx = self.conv_s1(x)
-        hx = self.bn_s1(hx)
-        hx = self.relu_s1(hx)
-        return hx
+        return self.block(x)
 
-# --------- Residual U-Blocks (RSU7 everywhere) ----------
-class RSU7(nn.Module):  # U-block with 7 layers
-    def __init__(self, in_ch=3, mid_ch=12, out_ch=3):
-        super(RSU7, self).__init__()
+# ------------------------
+#   RSU Block (Residual U-Block)
+# ------------------------
+class RSU(nn.Module):
+    def __init__(self, height, in_ch, mid_ch, out_ch):
+        super(RSU, self).__init__()
+        self.height = height
 
-        self.rebnconvin = REBNCONV(in_ch, out_ch)
+        self.in_conv = ConvBlock(in_ch, out_ch)
+        self.enc_blocks = nn.ModuleList()
+        self.pool = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.rebnconv1 = REBNCONV(out_ch, mid_ch)
-        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        # Encoder path
+        for i in range(height - 1):
+            if i == 0:
+                self.enc_blocks.append(ConvBlock(out_ch, mid_ch))
+            else:
+                self.enc_blocks.append(ConvBlock(mid_ch, mid_ch))
 
-        self.rebnconv2 = REBNCONV(mid_ch, mid_ch)
-        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        # Bottom
+        self.bottom = ConvBlock(mid_ch, mid_ch)
 
-        self.rebnconv3 = REBNCONV(mid_ch, mid_ch)
-        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        # Decoder path
+        self.dec_blocks = nn.ModuleList()
+        for _ in range(height - 1):
+            self.dec_blocks.append(ConvBlock(mid_ch * 2, mid_ch))
 
-        self.rebnconv4 = REBNCONV(mid_ch, mid_ch)
-        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
-
-        self.rebnconv5 = REBNCONV(mid_ch, mid_ch)
-        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
-
-        self.rebnconv6 = REBNCONV(mid_ch, mid_ch)
-        self.rebnconv7 = REBNCONV(mid_ch, mid_ch)
-
-        self.rebnconv6d = REBNCONV(mid_ch*2, mid_ch)
-        self.rebnconv5d = REBNCONV(mid_ch*2, mid_ch)
-        self.rebnconv4d = REBNCONV(mid_ch*2, mid_ch)
-        self.rebnconv3d = REBNCONV(mid_ch*2, mid_ch)
-        self.rebnconv2d = REBNCONV(mid_ch*2, mid_ch)
-        self.rebnconv1d = REBNCONV(mid_ch*2, out_ch)
+        self.out_conv = ConvBlock(mid_ch + out_ch, out_ch)
 
     def forward(self, x):
-        hxin = self.rebnconvin(x)
+        x_in = self.in_conv(x)
+        enc_feats = []
+        hx = x_in
 
-        hx1 = self.rebnconv1(hxin)
-        hx = self.pool1(hx1)
+        # Encoder
+        for i, block in enumerate(self.enc_blocks):
+            hx = block(hx)
+            enc_feats.append(hx)
+            if i < len(self.enc_blocks) - 1:
+                hx = self.pool(hx)
 
-        hx2 = self.rebnconv2(hx)
-        hx = self.pool2(hx2)
+        # Bottom
+        hx = self.bottom(hx)
 
-        hx3 = self.rebnconv3(hx)
-        hx = self.pool3(hx3)
+        # Decoder
+        for i, block in enumerate(self.dec_blocks):
+            skip = enc_feats[-(i + 1)]
+            hx = F.interpolate(hx, size=skip.shape[2:], mode="bilinear", align_corners=False)
+            hx = torch.cat([hx, skip], dim=1)
+            hx = block(hx)
 
-        hx4 = self.rebnconv4(hx)
-        hx = self.pool4(hx4)
+        # Output
+        hx = F.interpolate(hx, size=x_in.shape[2:], mode="bilinear", align_corners=False)
+        hx = torch.cat([hx, x_in], dim=1)
+        return self.out_conv(hx)
 
-        hx5 = self.rebnconv5(hx)
-        hx = self.pool5(hx5)
-
-        hx6 = self.rebnconv6(hx)
-        hx7 = self.rebnconv7(hx6)
-
-        hx6d = self.rebnconv6d(torch.cat((hx7, hx6), 1))
-        hx6dup = _upsample_like(hx6d, hx5)
-
-        hx5d = self.rebnconv5d(torch.cat((hx6dup, hx5), 1))
-        hx5dup = _upsample_like(hx5d, hx4)
-
-        hx4d = self.rebnconv4d(torch.cat((hx5dup, hx4), 1))
-        hx4dup = _upsample_like(hx4d, hx3)
-
-        hx3d = self.rebnconv3d(torch.cat((hx4dup, hx3), 1))
-        hx3dup = _upsample_like(hx3d, hx2)
-
-        hx2d = self.rebnconv2d(torch.cat((hx3dup, hx2), 1))
-        hx2dup = _upsample_like(hx2d, hx1)
-
-        hx1d = self.rebnconv1d(torch.cat((hx2dup, hx1), 1))
-
-        return hx1d + hxin
-
-# --------- U²-Net Main Architecture ----------
+# ------------------------
+#   U²-Net Model
+# ------------------------
 class U2NET(nn.Module):
     def __init__(self, in_ch=3, out_ch=1):
         super(U2NET, self).__init__()
-        self.stage1 = RSU7(in_ch, 32, 64)
+
+        # Encoder Stages
+        self.stage1 = RSU(7, in_ch, 32, 64)
         self.pool12 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.stage2 = RSU7(64, 32, 128)
+        self.stage2 = RSU(6, 64, 32, 128)
         self.pool23 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.stage3 = RSU7(128, 64, 256)
+        self.stage3 = RSU(5, 128, 64, 256)
         self.pool34 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.stage4 = RSU7(256, 128, 512)
+        self.stage4 = RSU(4, 256, 128, 512)
         self.pool45 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.stage5 = RSU7(512, 256, 512)
+        self.stage5 = RSU(4, 512, 256, 512)
         self.pool56 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.stage6 = RSU7(512, 256, 512)
+        self.stage6 = RSU(4, 512, 256, 512)
 
+        # Decoder Stages
+        self.stage5d = RSU(4, 1024, 256, 512)
+        self.stage4d = RSU(4, 1024, 128, 256)
+        self.stage3d = RSU(5, 512, 64, 128)
+        self.stage2d = RSU(6, 256, 32, 64)
+        self.stage1d = RSU(7, 128, 16, 64)
+
+        # Side Outputs
         self.side1 = nn.Conv2d(64, out_ch, 3, padding=1)
-        self.side2 = nn.Conv2d(128, out_ch, 3, padding=1)
-        self.side3 = nn.Conv2d(256, out_ch, 3, padding=1)
-        self.side4 = nn.Conv2d(512, out_ch, 3, padding=1)
+        self.side2 = nn.Conv2d(64, out_ch, 3, padding=1)
+        self.side3 = nn.Conv2d(128, out_ch, 3, padding=1)
+        self.side4 = nn.Conv2d(256, out_ch, 3, padding=1)
         self.side5 = nn.Conv2d(512, out_ch, 3, padding=1)
         self.side6 = nn.Conv2d(512, out_ch, 3, padding=1)
 
-        self.outconv = nn.Conv2d(6*out_ch, out_ch, 1)
+        self.outconv = nn.Conv2d(out_ch * 6, out_ch, 1)
 
     def forward(self, x):
+        # Encoder
         hx1 = self.stage1(x)
         hx = self.pool12(hx1)
 
@@ -136,23 +135,25 @@ class U2NET(nn.Module):
 
         hx6 = self.stage6(hx)
 
-        d1 = self.side1(hx1)
-        d2 = self.side2(hx2)
-        d3 = self.side3(hx3)
-        d4 = self.side4(hx4)
-        d5 = self.side5(hx5)
-        d6 = self.side6(hx6)
+        # Decoder
+        def cat_and_resize(a, b):
+            a = F.interpolate(a, size=b.shape[2:], mode="bilinear", align_corners=False)
+            return torch.cat([a, b], dim=1)
 
-        d2 = _upsample_like(d2, d1)
-        d3 = _upsample_like(d3, d1)
-        d4 = _upsample_like(d4, d1)
-        d5 = _upsample_like(d5, d1)
-        d6 = _upsample_like(d6, d1)
+        hx5d = self.stage5d(cat_and_resize(hx6, hx5))
+        hx4d = self.stage4d(cat_and_resize(hx5d, hx4))
+        hx3d = self.stage3d(cat_and_resize(hx4d, hx3))
+        hx2d = self.stage2d(cat_and_resize(hx3d, hx2))
+        hx1d = self.stage1d(cat_and_resize(hx2d, hx1))
 
-        d0 = self.outconv(torch.cat((d1, d2, d3, d4, d5, d6), 1))
+        # Side Outputs
+        d1 = self.side1(hx1d)
+        d2 = F.interpolate(self.side2(hx2d), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        d3 = F.interpolate(self.side3(hx3d), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        d4 = F.interpolate(self.side4(hx4d), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        d5 = F.interpolate(self.side5(hx5d), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        d6 = F.interpolate(self.side6(hx6), size=d1.shape[2:], mode="bilinear", align_corners=False)
 
+        # Final fused output
+        d0 = self.outconv(torch.cat([d1, d2, d3, d4, d5, d6], dim=1))
         return d0, d1, d2, d3, d4, d5, d6
-
-# --------- Utility for upsampling ----------
-def _upsample_like(src, tar):
-    return F.interpolate(src, size=tar.shape[2:], mode='bilinear', align_corners=False)
